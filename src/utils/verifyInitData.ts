296fb43d946cd0354a34a2c66ac35d163e06e9e2
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-10-05 02:47:23 +0300

    chore: formatting and peer deps

--- src/utils/verifyInitData.ts
+++ src/utils/verifyInitData.ts
@@ -1,26 +1,27 @@
-import crypto from "crypto";
+import crypto from "node:crypto";
 
 export function verifyTelegramInitData(initData: string, botToken: string) {
-  const parsed = new URLSearchParams(initData);
-  const hash = parsed.get("hash");
-  parsed.delete("hash");
+	const parsed = new URLSearchParams(initData);
+	const hash = parsed.get("hash");
+	parsed.delete("hash");
 
-  const dataCheckString = Array.from(parsed.entries())
-    .sort(([a], [b]) => a.localeCompare(b))
-    .map(([k, v]) => `${k}=${v}`)
-    .join("\n");
+	const dataCheckString = Array.from(parsed.entries())
+		.sort(([a], [b]) => a.localeCompare(b))
+		.map(([k, v]) => `${k}=${v}`)
+		.join("\n");
 
-  const secretKey = crypto
-    .createHmac("sha256", "WebAppData")
-    .update(botToken)
-    .digest();
-  const calculatedHash = crypto
-    .createHmac("sha256", secretKey)
-    .update(dataCheckString)
-    .digest("hex");
+	const secretKey = crypto
+		.createHmac("sha256", "WebAppData")
+		.update(botToken)
+		.digest();
+	const calculatedHash = crypto
+		.createHmac("sha256", secretKey)
+		.update(dataCheckString)
+		.digest("hex");
 
-  if (calculatedHash !== hash) throw new Error("Invalid Telegram signature");
+	if (calculatedHash !== hash) throw new Error("Invalid Telegram signature");
 
-  const user = JSON.parse(parsed.get("user")!);
-  return user;
+	const user = JSON.parse(parsed.get("user") ?? "{}");
+
+	return user;
 }
