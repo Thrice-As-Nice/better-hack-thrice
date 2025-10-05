a24de09f036b3185fc77ac6a8d5ea71e29d8a4a3
Ermias Sintayehu <ermiassintayehu13@gmail.com>
2025-10-05 03:08:58 +0300

    chore : move functions into seprate file

--- src/index.ts
+++ src/index.ts
@@ -1,119 +1,100 @@
-import type { BetterAuthPlugin, Schema } from "better-auth";
+import type { BetterAuthPlugin } from "better-auth";
 import { setSessionCookie } from "better-auth/cookies";
 import { createAuthEndpoint } from "better-auth/plugins";
-import type { User } from "better-auth/types";
 import * as z from "zod";
 import { verifyTelegramInitData } from "./utils/verifyInitData";
-
-function getOriginHostname(url: string) {
-	try {
-		const parsedUrl = new URL(url);
-		return parsedUrl.hostname;
-	} catch (_error) {
-		return null;
-	}
-}
-export interface TelegramMiniAppOptions {
-	botToken: string;
-	schema?: Schema;
-}
-
-export interface TelegramUserData extends User {
-	telegramId: string;
-	username: string;
-	photo_url: string;
-	is_premium: boolean;
-}
+import type { TelegramMiniAppOptions, TelegramUserData } from "./types";
+import { getOriginHostname } from "./utils/getOriginHostName";
 
 export const telegramMiniAppAuth = (
-	options: TelegramMiniAppOptions,
+  options: TelegramMiniAppOptions
 ): BetterAuthPlugin => {
-	return {
-		id: "telegram-mini-app",
-		endpoints: {
-			authenticate: createAuthEndpoint(
-				"/auth/telegram-mini-app",
-				{
-					method: "POST",
-					body: z.object({
-						initData: z.string().min(1),
-					}),
-					metadata: {
-						openapi: {
-							summary: "Authenticate Telegram Mini App",
-							description:
-								"Verifies Telegram Mini App initData and signs in or creates a user automatically.",
-							responses: {
-								200: {
-									description: "User authenticated successfully",
-								},
-							},
-						},
-					},
-				},
-				async (ctx) => {
-					const { initData } = ctx.body;
-					const { botToken } = options;
+  return {
+    id: "telegram-mini-app",
+    endpoints: {
+      authenticate: createAuthEndpoint(
+        "/auth/telegram-mini-app",
+        {
+          method: "POST",
+          body: z.object({
+            initData: z.string().min(1),
+          }),
+          metadata: {
+            openapi: {
+              summary: "Authenticate Telegram Mini App",
+              description:
+                "Verifies Telegram Mini App initData and signs in or creates a user automatically.",
+              responses: {
+                200: {
+                  description: "User authenticated successfully",
+                },
+              },
+            },
+          },
+        },
+        async (ctx) => {
+          const { initData } = ctx.body;
+          const { botToken } = options;
 
-					const userData = verifyTelegramInitData(initData, botToken);
-					const domain = getOriginHostname(ctx.context.baseURL);
-					const userEmail = `telegram-${userData.id}@${domain}`;
+          const userData = verifyTelegramInitData(initData, botToken);
+          const domain = getOriginHostname(ctx.context.baseURL);
+          const userEmail = `telegram-${userData.id}@${domain}`;
 
-					let user = await ctx.context.adapter.findOne<TelegramUserData>({
-						model: "user",
-						where: [{ field: "telegramId", value: userData.id.toString() }],
-					});
+          let user = await ctx.context.adapter.findOne<TelegramUserData>({
+            model: "user",
+            where: [{ field: "telegramId", value: userData.id.toString() }],
+          });
 
-					if (!user) {
-						user = await ctx.context.internalAdapter.createUser(
-							{
-								telegramId: userData.id.toString(),
-								name: `${userData.first_name} ${
-									userData.last_name || ""
-								}`.trim(),
-								email: userEmail,
-								emailVerified: false,
-								createdAt: new Date(),
-								updatedAt: new Date(),
-								username: userData.username,
-								photoUrl: userData.photo_url,
-								isPremium: userData.is_premium || false,
-							},
-							ctx,
-						);
-					}
+          if (!user) {
+            user = await ctx.context.internalAdapter.createUser(
+              {
+                telegramId: userData.id.toString(),
+                name: `${userData.first_name} ${
+                  userData.last_name || ""
+                }`.trim(),
+                email: userEmail,
+                emailVerified: false,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                username: userData.username,
+                photoUrl: userData.photo_url,
+                isPremium: userData.is_premium || false,
+              },
+              ctx
+            );
+          }
 
-					const newSession = await ctx.context.internalAdapter.createSession(
-						user.id,
-						ctx,
-						false,
-						ctx.context.session?.session,
-					);
+          const newSession = await ctx.context.internalAdapter.createSession(
+            user.id,
+            ctx,
+            false,
+            ctx.context.session?.session
+          );
 
-					await setSessionCookie(ctx, {
-						session: newSession,
-						user,
-					});
+          await setSessionCookie(ctx, {
+            session: newSession,
+            user,
+          });
 
-					return ctx.json({
-						user,
-						session: newSession,
-					});
-				},
-			),
-		},
+          return ctx.json({
+            user,
+            session: newSession,
+          });
+        }
+      ),
+    },
 
-		schema: {
-			user: {
-				fields: {
-					telegramId: { type: "string", unique: true },
-					isPremium: { type: "boolean", required: false },
-					firstName: { type: "string", required: true },
-					lastName: { type: "string", required: false },
-					username: { type: "string", required: false },
-					photoUrl: { type: "string", required: false },
-				},
-			},
-		},
-	};
+    schema: {
+      user: {
+        fields: {
+          telegramId: { type: "string", unique: true },
+          isPremium: { type: "boolean", required: false },
+          firstName: { type: "string", required: true },
+          lastName: { type: "string", required: false },
+          username: { type: "string", required: false },
+          photoUrl: { type: "string", required: false },
+        },
+      },
+    },
+  };
 };
