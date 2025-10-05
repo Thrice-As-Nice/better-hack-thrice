86334a245d5b0ae337012b5a8a6f22fd0ed7d9de
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-10-05 04:16:30 +0300

    chore: formatting

--- src/utils/getOriginHostName.ts
+++ src/utils/getOriginHostName.ts
@@ -1,8 +1,8 @@
 export const getOriginHostname = (url: string) => {
-  try {
-    const parsedUrl = new URL(url);
-    return parsedUrl.hostname;
-  } catch (_error) {
-    return null;
-  }
+	try {
+		const parsedUrl = new URL(url);
+		return parsedUrl.hostname;
+	} catch (_error) {
+		return null;
+	}
 };
